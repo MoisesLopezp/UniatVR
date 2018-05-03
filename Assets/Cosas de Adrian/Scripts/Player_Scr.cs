1ec3711c73188f7da077f6083c01c2c1f14d1314
Alain <barraganalain55@gmail.com>
2018-05-03 11:27:24 -0500

    Revert "Merge branch 'master' of https://github.com/MoisesLopezp/UniatVR"
    This reverts commit ba33b545037bcb65e9362b09110356dd392b8fdc, reversing
    changes made to 45e2ed0974fb071157f9b1e3d87c0dec5fe1ecb4.

--- Assets/Cosas de Adrian/Scripts/Player_Scr.cs
+++ Assets/Cosas de Adrian/Scripts/Player_Scr.cs
@@ -1,19 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-/////////////////////DEBUG///////////////
-using UnityEngine.UI;
 
 public class Player_Scr : MonoBehaviour
 {
     Transform camara_transform;
     MenuPause_Scr pausa;
-<<<<<<< HEAD
-=======
-
-    /////////////////////DEBUG////////////////
-    public Text text;
->>>>>>> d74cb3d7d2204ed2d037cddc12deaffb558f483c
 
     private void Start()
     {
@@ -26,23 +18,7 @@ public class Player_Scr : MonoBehaviour
         this.transform.Translate(camara_transform.forward * Time.deltaTime * 3 * Input.GetAxis("Vertical"));
         this.transform.Translate(camara_transform.right * Time.deltaTime * 3 * Input.GetAxis("Horizontal"));
 
-<<<<<<< HEAD
         if (Input.GetButtonDown("Jump"))
             pausa.Pausa();
-=======
-        if (Input.GetKeyDown(KeyCode.P))
-            pausa.Pausa();
-
-        /////////////////////DEBUG///////////////
-        if (Input.GetButton("Fire1"))
-            text.text = "Fire1";
-        if (Input.GetButton("Fire2"))
-            text.text = "Fire2";
-        if (Input.GetButton("Fire3"))
-            text.text = "Fire3";
-        if (Input.GetButton("Jump"))
-            text.text = "jump";
-
->>>>>>> d74cb3d7d2204ed2d037cddc12deaffb558f483c
     }
 }
